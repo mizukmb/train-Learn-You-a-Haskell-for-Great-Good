d48358e29db07da3a66d0f9501f5875ab0f1a0dc
mizukmb <mizukmb6@gmail.com>
2017-12-03 17:24:43 +0900

    RPN 関数を書く

---
+++ chapter10/rpn.hs
@@ -0,0 +1,7 @@
+solveRPN :: String -> Double
+-- solveRPN expression = head (foldl foldingFunction [] (words expression))
+solveRPN = head . foldl foldingFunction [] . words
+    where foldingFunction (x:y:ys) "*"    = (y * x):ys
+          foldingFunction (x:y:ys) "+"    = (y + x):ys
+          foldingFunction (x:y:ys) "-"    = (y - x):ys
+          foldingFunction xs numberString = read numberString:xs
